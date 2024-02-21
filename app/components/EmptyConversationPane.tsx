4fcfbf7643b58087c3788c6547204fc1e9aba718
Apoorv Agarwal <apoorvagarwal@Apoorvs-MacBook-Air.local>
2024-02-21 17:27:47 +0530

    fixed friends text

--- app/components/EmptyConversationPane.tsx
+++ app/components/EmptyConversationPane.tsx
@@ -4,7 +4,7 @@ const EmptyConversationPane = () => {
         <div className="min-h-full bg-stone-50 flex flex-col items-center justify-center">
             <span className="text-lg font-semibold text-gray-900">Hi,{}</span>
             <span className="text-sm text-gray-900">Start a new conversation.</span>
-            <p className="text-sm text-gray-900 w-60 text-wrap">Everyone is a friend here. I will bring the ability to choose whom you want to talk in some time.</p>
+            <p className="text-sm text-gray-900 w-80 text-wrap text-center">Everyone is a friend here. I will bring the ability to choose whom you want to talk in some time.</p>
         </div>
     )
 }
